f8ab963033b3d1d4333f6fae60084a7920a577cc
helman101 <helmanandresortegon@gmail.com>
2021-01-20 12:29:12 +0100

    add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validations :name, lenght: { minimum: 5 }, presence: true, uniqueness: true
+  validations :password, lenght: { minimum: 8 }, presence: true
+  validations :email, length: { minumum: 10 }, presence: true, uniqueness: true
 end
