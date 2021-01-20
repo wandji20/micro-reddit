66dae78930f8a7267bb4e77d84cdf4b08995afd9
helman101 <helmanandresortegon@gmail.com>
2021-01-20 12:35:52 +0100

    update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validations :name, lenght: { minimum: 5 }, presence: true, uniqueness: true
-  validations :password, lenght: { minimum: 8 }, presence: true
-  validations :email, length: { minumum: 10 }, presence: true, uniqueness: true
+  validates :name, length: { minimum: 5 }, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, presence: true
+  validates :email, length: { minimum: 10 }, presence: true, uniqueness: true
 end
