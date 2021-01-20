bd4ef0147ce445fef6248bb345f1bf6b605ac759
wandji20 <wbertrand10@gmail.com>
2021-01-20 16:12:49 +0100

    Update readme file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_143829) do
+ActiveRecord::Schema.define(version: 2021_01_20_135325) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -18,9 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_20_143829) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -41,6 +39,5 @@ ActiveRecord::Schema.define(version: 2021_01_20_143829) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
